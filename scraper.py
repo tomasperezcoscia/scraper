7495496c2940835235979b5bc6356fbc9e8225de
tomasperezcoscia <71987424+tomasperezcoscia@users.noreply.github.com>
2024-02-22 20:55:38 -0300

    [GENE] Add scraper.py, modify sitemap.py, add sitemapInitial.py, delete urls.json and urls.txt

---
+++ scraper.py
@@ -0,0 +1,124 @@
+import requests
+import json
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+
+urls = []
+
+with open('urls.json', 'r') as f:
+	urls = json.loads(f.read())
+
+driver = webdriver.Chrome()
+
+def Get_Element_Text(driver, selector, mode='css'):
+	try:
+		if(mode == 'css'):
+			element = driver.find_element(By.CSS_SELECTOR, selector)
+		else:
+			element = driver.find_element(By.XPATH, selector)
+
+		if(element is not None):
+			return element.get_attribute('innerHTML')
+		else:
+			return False
+	except:
+		print('NO SE LOGRO ENCONTRAR EL ELEMENTO -> {0}'.format(selector))
+		return False
+
+def Get_Page_Mode(driver):
+	try:
+		wait = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".styles__AddressWrapper-fs-hdp__sc-13x5vko-0 > h1")))
+		return 'horizontal'
+	except:
+		return 'vertical'
+
+def Scrape_Page_Data(mode):
+	data = {}
+	if(mode == 'horizontal'):
+		print('Scrapeando data para modo horizontal')
+		data['bedrooms'] = Get_Element_Text(driver, '/html/body/div[1]/div/div[2]/div/div[1]/div[2]/div[1]/div[1]/div[3]/div[1]/div/div/div[2]/div/div/div/div/div[1]/div/div[2]/div/div[1]/span[1]', 'xpath')
+		data['bathrooms'] = Get_Element_Text(driver, '/html/body/div[1]/div/div[2]/div/div[1]/div[2]/div[1]/div[1]/div[3]/div[1]/div/div/div[2]/div/div/div/div/div[1]/div/div[2]/div/button/div/span[1]', 'xpath')
+		data['squarefoot'] = Get_Element_Text(driver, '/html/body/div[1]/div/div[2]/div/div[1]/div[2]/div[1]/div[1]/div[3]/div[1]/div/div/div[2]/div/div/div/div/div[1]/div/div[2]/div/div[2]/span[1]', 'xpath')
+		data['price'] = Get_Element_Text(driver, 'span[data-testid="price"] > span')
+		data['direction'] = Get_Element_Text(driver, '.styles__AddressWrapper-fs-hdp__sc-13x5vko-0 > h1')
+		data['description'] = Get_Element_Text(driver, 'article .Text-c11n-8-99-3__sc-aiai24-0')
+
+		data['features'] = []
+		data['photos_url'] = []
+		features = driver.find_elements(By.CSS_SELECTOR, '.styles__StyledContainer-fs-hdp__sc-6k0go5-0 span')
+		for feature in features:
+			data['features'].append(feature.text.strip())
+
+		try:
+			btn = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div/div[1]/div[2]/div[1]/div[1]/div[2]/div[2]/div/div[1]/div/button')
+			btn.click()
+
+			print('BUTTON DATA ->')
+			print(btn)
+			time.sleep(1)
+			total_photos = driver.find_elements(By.CSS_SELECTOR, 'picture source')
+			
+			for photo in total_photos:
+				data['photos_url'].append(photo.get_attribute('srcset'))
+		except Exception as e:
+			print('exploto obteniendo fotos ->')
+			print(e)
+
+	else:
+		print('Scrapeando data para modo vertical')
+
+	return data
+
+for new_url in urls:
+
+	#NAVEGAMOS A LA URL
+	print('Scrapeando la URL -> {0}'.format(new_url))
+	driver.get(new_url)
+
+	#SCRIPT PARA SCROLLEAR
+	driver.execute_script('''
+    var element = document.querySelector('.layout-container-desktop');
+    if (element) {
+        element.scrollTop += 50; // Scroll down by 50 pixels
+    }
+	''')
+
+	#ESPERAMOS LA PRESENCIA DE ELEMENTOS PARA EVITAR CRASHEOS
+	try:
+		page_mode = Get_Page_Mode(driver)
+		page_data = Scrape_Page_Data(page_mode)
+		print('DATA DE ESTA PAGINA -> ')
+		print(page_data)
+		input()
+		#direction = Get_Element_Text(driver, '.styles__AddressWrapper-fs-hdp__sc-13x5vko-0 > h1')
+	except:
+		print('HUBO UN ERROR ESPERANDO AL ELEMENTO.-')
+		continue
+
+	driver.delete_all_cookies()
+	time.sleep(2)
+
+input('gorreado')
+
+
+'''
+
+FACTORES BASICOS :
+
+
+CARACTERISTICAS RANDOM
+.styles__StyledContainer-fs-hdp__sc-1dg6897-0 (CONTAINER)
+	-> DIV (SUBCONTAINER) -> SPAN (CARACTERISTICA)
+
+INSIGHTS
+.InsightsTagsstyles__TagContainer-fs-hdp__sc-rgxjfy-0 (CONTAINER) -> SPAN = CARACTERISTICA INDIVUAL (EXTRAER TEXTO)
+
+
+Text-c11n-8-99-3__sc-aiai24-0 -> DESCRIPCION.-
+
+'''
--- sitemap.py
+++ sitemap.py
@@ -1,65 +1,58 @@
 import requests
 import json
-from bs4 import BeautifulSoup
 
-# URL of the XML file
-url = "https://www.zillow.com/xml/sitemaps/us/hdp/for-sale-by-owner/latest.xml.gz"
+r = requests.get("https://www.zillow.com/xml/sitemaps/us/hdp/for-sale-by-owner/latest.xml.gz")
 
-# Send an HTTP request to the URL
-response = requests.get(url)
-
-# Check if the request was successful
-if response.status_code == 200:
-    # Parse the XML data using BeautifulSoup with 'html.parser'
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    # Find all <loc> tags
-    loc_tags = soup.find_all('loc')
-
-    # Extract URLs
-    urls = [loc.text.strip() for loc in loc_tags]
-
-    # Print the URLs
-    for url in urls:
-        print(url)
-        print("\n")
-        
-    print("Finished printing the URLs")  
-    total_urls = len(urls)
-    print("Total number of URLs:", total_urls)  
-
-    array_converted = json.dumps(urls)
-
-    f = open('urls.json', 'w')
-    f.write(array_converted)
-    f.close()
-
-    
-
-
+if(r.status_code != 200 and r.status_code != '200'):
+	print('FALLO LA REQUEST.-')
+	input()
 else:
-    print("Failed to retrieve data from the URL:", response.status_code)
+	print('REQUEST SUCCESSFULLY')
+
+arr = r.text.splitlines()
+
+total_urls = []
+
+for elem in arr:
+	if('<loc>' in elem and '</loc>' in elem):
+		url_trimmed = elem.strip()
+		url_trimmed = url_trimmed.replace('<loc>', '')
+		url_trimmed = url_trimmed.replace('</loc>', '')
+		total_urls.append(url_trimmed)
+
+print('FINISHED')
+print(total_urls[0])
 
 
-    '''
-    ELEMENTO[0] -> NUMERO DE ELEMENTOS
+array_converted = json.dumps(total_urls)
 
-    data-testid="bed-bath-sqft-fact-container"
-    bed-bath-sqft-fact-container[0] -> Habitaciones [0] -> Cant de habitaciones
-    bed-bath-sqft-fact-container[1] -> Baños [0] -> Cant de baños
-    bed-bath-sqft-fact-container[2] -> M2 [0] -> Cant de m2
+f = open('urls.json', 'w')
+f.write(array_converted)
+f.close()
 
-    data-testid="price" -> Precio
-
-    class="styles__AddressWrapper-fs-hdp__sc-13x5vko-0" > h1 .innerHTML -> Dirección
-
-    class="styles__StyledContainer-fs-hdp__sc-1dg6897-0" -> Datos random (CONTAINER)
-        ->Div 
-            ->SPAN (CARACTERISTICA)
-            
-
-    class="Text-c11n-8-99-3__sc-aiai24-0" -> Descripción      
-            '''
+input('Presione cualquier tecla para finalizar')
 
 
 
+'''
+
+FACTORES BASICOS :
+
+data-testid="bed-bath-sqft-fact-container"[0] -> SPAN -> HABITACIONES
+data-testid="bed-bath-sqft-fact-container"[1] -> SPAN -> BAÑOS
+data-testid="bed-bath-sqft-fact-container"[2] -> SPAN -> SQUARE FOOT
+
+data-testid="price" -> SPAN -> Precio (EN USD)
+.styles__AddressWrapper-fs-hdp__sc-13x5vko-0 > h1 -> DIRECCION
+
+CARACTERISTICAS RANDOM
+.styles__StyledContainer-fs-hdp__sc-1dg6897-0 (CONTAINER)
+	-> DIV (SUBCONTAINER) -> SPAN (CARACTERISTICA)
+
+INSIGHTS
+.InsightsTagsstyles__TagContainer-fs-hdp__sc-rgxjfy-0 (CONTAINER) -> SPAN = CARACTERISTICA INDIVUAL (EXTRAER TEXTO)
+
+
+Text-c11n-8-99-3__sc-aiai24-0 -> DESCRIPCION.-
+
+'''
